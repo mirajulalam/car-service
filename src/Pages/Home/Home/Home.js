3ebbdd62de6fc5528fd35904c10c6f8208e42e4a
mirajulalam <alifmirajulalam@gmail.com>
2023-04-07 16:08:04 +0600

    banner components added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import About from '../About/About';
 
 const Home = () => {
     return (
         <div>
-            <h2>home</h2>
+            <Banner></Banner>
+            <About></About>
         </div>
     );
 };
